40ab877c77463d8e28c02ac13ea76f53b1c8d5ef
Akshay <itstooeasy74@gmail.com>
2023-10-31 15:44:43 +0530

    30OCT23N

--- 30OCT23/Task6.py
+++ 30OCT23/Task6.py
@@ -1,33 +1,14 @@
-# Convert to Dict JSON Response and Print and Booking Id AND checkin and Checkout Data
-import json
 
-json_response='''
-{
-"bookingid": 2384,
-"booking": {
-"firstname": "PRAMOD",
-"lastname": "Dutta",
-"totalprice": 432,
-"depositpaid": False,
-"bookingdates": {
-"checkin": "2022-01-01",
-"checkout": "2022-01-01"
-},
-"additionalneeds": "Lunch"
-}
-}
-'''
+dict={}
 
-my_string=json_response
-#my_string = my_string.replace("\n", "")
-#my_string = my_string.replace(" ", "")
-my_string = my_string.replace("False","false")
-my_string = my_string.replace("True","true")
-print(my_string)
-json_dict = json.loads(my_string)
-print(json_dict)
-print("Booking Id is :",json_dict.get("bookingid"))
-print("Check In date is : ",json_dict.get("booking").get('bookingdates').get('checkin'))
-print("Check In date is : ",json_dict["booking"]["bookingdates"]["checkin"])
-print("Check out date is : ",json_dict.get("booking").get('bookingdates').get('checkout'))
-print("Check In date is : ",json_dict["booking"]["bookingdates"]["checkout"])
+while True:
+    res=input("Do you want to enter Y/N : ")
+    if res=='Y' or res=="y":
+        k=input("enter key : ")
+        v=input("enter value : ")
+        dict.update({k:v})
+    else:
+        break
+
+print(dict)
+
