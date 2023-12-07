cbb0cb2e78e2150cbe855e92327f59b9373ccd6c
Akshay <itstooeasy74@gmail.com>
2023-12-07 19:34:41 +0530

    6DECNN

--- 6DEC23/Task2.py
+++ 6DEC23/Task2.py
@@ -4,7 +4,7 @@ import pytest
 url = "https://restful-booker.herokuapp.com./booking/"
 tokenurl = "https://restful-booker.herokuapp.com/auth"
 
-
+@pytest.mark.positive
 def test_tokencreate():
     head = {
         "Content-Type": "application/json",
@@ -20,8 +20,11 @@ def test_tokencreate():
     global token
     tokendata = data["token"]
     token="token="+tokendata
+    assert res.status_code == 200, "TC1 Failed"
+    assert "token" in data, "TC2 failed"
+    assert res.elapsed.microseconds < 1000000, "TC3 Failed"
 
-
+@pytest.mark.positive
 def test_create():
     head = {}
     payload = {
@@ -40,13 +43,26 @@ def test_create():
     data = res.json()
     global bookid
     bookid = data["bookingid"]
+    assert res.status_code == 200, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data["bookingid"] is not None, "TC3 failed"
+    assert data["booking"]["firstname"] is not None, "TC4 failed"
+    assert data["booking"]["firstname"]=="pat", "TC5 failed"
 
 
+@pytest.mark.positive
 def test_read():
     res = req.get(url=url + str(bookid))
     print(res.json())
+    data=res.json()
+    assert res.status_code == 200, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data["firstname"] is not None, "TC3 failed"
+    assert data["lastname"] is not None, "TC4 failed"
+    assert data["firstname"] == "pat", "TC5 failed"
 
 
+@pytest.mark.positive
 def test_fullupdate():
     head = {
         "Content-Type": "application/json",
@@ -66,8 +82,16 @@ def test_fullupdate():
     }
 
     res = req.put(url=url + str(bookid), headers=head, json=payload)
+    data=res.json()
     print(res.json())
+    assert res.status_code == 200, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data["firstname"] is not None, "TC3 failed"
+    assert data["lastname"] is not None, "TC4 failed"
+    assert data["firstname"] == "Raju", "TC5 failed"
 
+
+@pytest.mark.negative
 def test_partialupdate():
     head = {
         "Content-Type": "application/json",
@@ -81,7 +105,14 @@ def test_partialupdate():
 
     res = req.patch(url=url + str(bookid),headers=head,json=payload)
     print(res.json())
+    data=res.json()
+    assert res.status_code == 200, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data["firstname"] is not None, "TC3 failed"
+    assert data["lastname"] is not None, "TC5 failed"
+    assert data["firstname"] == "Raju", "TC5 failed"
 
+@pytest.mark.positive
 def test_delete():
     head = {
         "Content-Type": "application/json",
@@ -90,3 +121,7 @@ def test_delete():
     }
     res=req.delete(url+str(bookid),headers=head)
     print(res.text)
+    data=res.text
+    assert res.status_code == 201, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data == "Created", "TC3 failed"
--- 6DEC23/Task3.py
+++ 6DEC23/Task3.py
@@ -1,29 +1,28 @@
-import requests as req
 import pytest
+import requests as req
 
 url = "https://restful-booker.herokuapp.com./booking/"
 tokenurl = "https://restful-booker.herokuapp.com/auth"
 
-
-def test_tokencreate():
-    head = {
-        "Content-Type": "application/json",
-        "Accept": "*/*"
-    }
+@pytest.mark.positive
+def test_tokenCreate():
+    header = {}
     payload = {
         "username": "admin",
         "password": "password123"
     }
-    res = req.post(url=tokenurl, headers=head, json=payload)
+    res = req.post(url=tokenurl, headers=header, json=payload)
     data = res.json()
     print(data)
-    global token
-    tokendata = data["token"]
-    token="token="+tokendata
+    token = data["token"]
+    assert res.status_code==200, "TC1 Failed"
+    assert "token" in data, "TC2 failed"
+    assert res.elapsed.microseconds<1000000 , "TC3 Failed"
+    return token
 
-
+@pytest.mark.positive
 def test_create():
-    head = {}
+    header = {}
     payload = {
         "firstname": "pat",
         "lastname": "Brown",
@@ -35,58 +34,89 @@ def test_create():
         },
         "additionalneeds": "Breakfast"
     }
-    res = req.post(url=url, headers=head, json=payload)
-    print(res.json())
+    res = req.post(url=url, headers=header, json=payload)
     data = res.json()
-    global bookid
+    print(data)
     bookid = data["bookingid"]
+    assert res.status_code==200, "TC1 failed"
+    assert res.elapsed.microseconds<1000000, "TC2 failed"
+    assert data["bookingid"] is not None, "TC3 failed"
+    assert data["booking"]["firstname"] is not None, "TC4 failed"
+    assert data["booking"]["firstname"]=="pat", "TC5 failed"
+    return bookid
 
-
+@pytest.mark.positive
 def test_read():
-    res = req.get(url=url + str(bookid))
-    print(res.json())
+    header = {}
+    payload = {}
 
+    res = req.get(url=url + str(test_create()))
+    data = res.json()
+    print(data)
+    assert res.status_code == 200, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data["firstname"] is not None, "TC3 failed"
+    assert data["lastname"] is not None, "TC4 failed"
+    assert data["firstname"]=="pat", "TC5 failed"
 
-def test_fullupdate():
-    head = {
-        "Content-Type": "application/json",
-        "Accept": "*/*",
-        "Cookie": token
+@pytest.mark.positive
+def test_fullUpdate():
+    token="token="+test_tokenCreate()
+    header = {
+        "Cookie" : token
     }
     payload = {
-        "firstname": "Raju",
-        "lastname": "Brown",
-        "totalprice": 111,
-        "depositpaid": True,
-        "bookingdates": {
-            "checkin": "2018-01-01",
-            "checkout": "2019-01-01"
-        },
-        "additionalneeds": "Breakfast"
-    }
+            "firstname": "Raju",
+            "lastname": "Brown",
+            "totalprice": 111,
+            "depositpaid": True,
+            "bookingdates": {
+                "checkin": "2018-01-01",
+                "checkout": "2019-01-01"
+            },
+            "additionalneeds": "Breakfast"
+        }
+    res = req.put(url=url + str(test_create()), headers=header, json=payload)
+    data = res.json()
+    print(data)
+    assert res.status_code == 200, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data["firstname"] is not None, "TC3 failed"
+    assert data["lastname"] is not None, "TC4 failed"
+    assert data["firstname"]=="Raju", "TC5 failed"
 
-    res = req.put(url=url + str(bookid), headers=head, json=payload)
-    print(res.json())
 
-def test_partialupdate():
-    head = {
-        "Content-Type": "application/json",
-        "Accept": "*/*",
-        "Cookie": token
+@pytest.mark.negative
+def test_partialUpdate():
+    token="token="+test_tokenCreate()
+    header={
+        "Cookie" : token
     }
-    payload = {
+    payload={
         "firstname": "Raju",
         "lastname": "Herapheri",
+        "totalprice":18181818
     }
+    res=req.patch(url=url+str(test_create()),headers=header,json=payload)
+    data=res.json()
+    print(data)
+    assert res.status_code == 200, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data["firstname"] is not None, "TC3 failed"
+    assert data["lastname"] is not None, "TC5 failed"
+    assert data["firstname"]=="Raju", "TC5 failed"
 
-    res = req.patch(url=url + str(bookid),headers=head,json=payload)
-    print(res.json())
 
+@pytest.mark.positive
 def test_delete():
-    head = {
-        "Content-Type": "application/json",
-        "Accept": "*/*",
-        "Cookie": token
+    token="token="+test_tokenCreate()
+    header={
+        "Cookie" : token
     }
-    res=req.delete(url+str(bookid),headers=head)
-    print(res.text)
+    payload={}
+    res=req.delete(url=url+str(test_create()),headers=header)
+    data=res.text
+    print(data)
+    assert res.status_code == 201, "TC1 failed"
+    assert res.elapsed.microseconds < 1000000, "TC2 failed"
+    assert data=="Created", "TC3 failed"
